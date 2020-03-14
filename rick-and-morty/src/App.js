5a6c702c6f718a86d81bf388c9e6194b1fbab750
Os24 <os.deleyja@gmail.com>
2020-03-14 15:12:51 -0600

    changing some css

--- rick-and-morty/src/App.js
+++ rick-and-morty/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  
+  renderCards(){
+    return(
+      <div className = 'Card'>
+      <div className='Card-image'>
+        <figure>
+        <img src ={'https://rickandmortyapi.com/api/character/avatar/25.jpeg'} alt="rick and morty"/>
+        </figure>
+      </div>
+      <div className='Card-description'>
+        <div className='Card-name'>
+          <h3>Rick</h3>
+        </div>
+      </div>
+  </div>
+    )
+  }
   render(){
-    return (
+
+    const fakeData = [0,1,2,3,4,5,6,7]
+
+    const cards = fakeData.map(e => this.renderCards())
+    console.log(cards,"cards")
+      return (
       <div className="App">
         <div className='App-contenedor'>
           <h1>Hello world</h1>
           <div className='Cards-container'>
-            <div className = 'Card'>
-                <div className='Card-image'>
-                  <figure>
-                  <img src ={'https://rickandmortyapi.com/api/character/avatar/25.jpeg'}/>
-                  </figure>
-                </div>
-                <div className='Card-description'>
-                  <div className='Card-name'>
-                    <h3>Rick sabdjksahdsjkahdjksahdkjsahkd</h3>
-                  </div>
-                </div>
-            </div>
+            {cards}
           </div>
         </div>
       </div>
