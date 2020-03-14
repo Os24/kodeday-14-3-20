18bdae9adfbaae110953ff35e0d0874cd3c2db2f
oslo24 <oscar.lopez@anzen.com.mx>
2020-03-14 13:25:36 -0600

    some commit

--- rick-and-morty/src/App.js
+++ rick-and-morty/src/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <div className='App-contenedor'>
+          <h1>Hello world</h1>
+          <div className='Cards-container'>
+            <div className = 'Card'>
+                <div className='Card-image'>
+                  <figure>
+                  <img src ={'https://rickandmortyapi.com/api/character/avatar/25.jpeg'}/>
+                  </figure>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+
 }
 
 export default App;
